22782f3ff7214255b9e140ee227cb4814a24b14a
movahedi <movahedi_821@yahoo.com>
2024-01-11 21:46:27 +0330

    add loading and debouncing for apiFetcherComponent

--- inlab_plasmic/next.config.js
+++ inlab_plasmic/next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   trailingSlash: true,
+  // https://github.com/vercel/next.js/issues/58817#issuecomment-1868127500
+  transpilePackages: [
+    'antd',
+    '@ant-design',
+    'rc-util',
+    'rc-pagination',
+    'rc-picker',
+    'rc-notification',
+    'rc-tooltip',
+    "rc-progress",
+  ]
 };
 
 module.exports = nextConfig;
--- inlab_plasmic/utils/ApiFetcherComponent.tsx
+++ inlab_plasmic/utils/ApiFetcherComponent.tsx
@@ -1,8 +1,9 @@
-import { ReactNode, useEffect, useState, useContext, useMemo } from 'react';
-import { CodeComponentMeta, DataProvider, GlobalActionsProvider, useSelector } from '@plasmicapp/react-web/lib/host';
+import { ReactNode, useEffect, useState, useContext } from 'react';
+import { CodeComponentMeta, DataProvider, useSelector } from '@plasmicapp/react-web/lib/host';
 import axios from 'axios';
 import { refreshAccessIfNeeded, logForDev } from './CommonUtils';
 import { GlobalContext } from './CommonTypes';
+import { Progress } from 'antd';
 
 interface PropsType {
     className?: string,
@@ -11,6 +12,7 @@ interface PropsType {
     path: string,
     headers?: object,
     requestBody?: object,
+    delay?: number,
 }
 
 // TODO: We may need to handle axios race condition
@@ -21,37 +23,62 @@ export function ApiFetcherComponent(props: PropsType) {
     const inlabUser = useSelector('inlab_user');
 
     const [data, setData] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    const twoColors = { "0%": "#108ee9", "100%": "#87d068" };
 
     useEffect(() => {
-        logForDev("ApiFetcherComponent: useEffect: runned: " + (inlabUser ? "User found" : "User is NULL"));
-        // TODO: Handle errors
-        inlabUser && refreshAccessIfNeeded(globalContext, inlabUser).then(user => {
-            if (!user) {
-                return;
-            }
+        const control = new AbortController();
+        const searchTimeOut = setTimeout(() => {
 
-            const authedHeaders: object = {
-                'Authorization': 'Bearer ' + user.access,
-                ...props.headers
-            };
+            logForDev("ApiFetcherComponent: useEffect: runned: " + (inlabUser ? "User found" : "User is NULL"));
+            // TODO: Handle errors
+            inlabUser && refreshAccessIfNeeded(globalContext, inlabUser).then(user => {
+                if (!user) {
+                    return;
+                }
 
-            axios.request({
-                method: props.method || 'GET',
-                url: globalContext.baseUrl + props.path,
-                headers: authedHeaders,
-                data: props.requestBody
-            }).then((response) => {
-                logForDev("ApiFetcherComponent: axios request success: " + (response.data ? "Data Fetched" : "Data Not Fetched"));
-                setData(response);
-            }).catch((error) => {
-                logForDev("ApiFetcherComponent: axios request error: " + JSON.stringify(error));
-                setData(error);
-            })
-        }).catch(error => { });
+                const authedHeaders: object = {
+                    'Authorization': 'Bearer ' + user.access,
+                    ...props.headers
+                };
+
+                setLoading(true);
+
+                axios.request({
+                    method: props.method || 'GET',
+                    url: globalContext.baseUrl + props.path,
+                    headers: authedHeaders,
+                    data: props.requestBody,
+                    signal: control.signal,
+                }).then((response) => {
+                    logForDev("ApiFetcherComponent: axios request success: " + (response.data ? "Data Fetched" : "Data Not Fetched"));
+                    setLoading(false);
+                    setData(response);
+                }).catch((error) => {
+                    if (axios.isCancel(error)) {
+                        return;
+                    }
+                    logForDev("ApiFetcherComponent: axios request error: " + JSON.stringify(error));
+                    setLoading(false);
+                    setData(error);
+                })
+
+
+
+            }).catch(error => { });
+
+        }, props.delay || 0);
+
+        return () => {
+            control.abort();
+            clearTimeout(searchTimeOut);
+        }
     }, [props, globalContext, inlabUser]);
 
     return (
         <div className={props.className}>
+            {loading && <Progress type="line" percent={100} showInfo={false} status="active" strokeColor={twoColors} strokeLinecap="butt" />}
             <DataProvider name="fetched_data" data={data}>
                 {props.children}
             </DataProvider>
@@ -71,6 +98,7 @@ export const ApiFetcherMeta: CodeComponentMeta<PropsType> = {
         path: 'string',
         headers: 'object',
         requestBody: 'object',
+        delay: 'number',
     },
     providesData: true,
     importPath: "./utils/ApiFetcherComponent",
