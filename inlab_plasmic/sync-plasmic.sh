43c6237b5189560396f0a500ed4264eafefffd9c
royarayati <royarayati1379@gmail.com>
2025-02-01 17:35:07 +0330

    replacing project_id plasmic in sync file

--- inlab_plasmic/sync-plasmic.sh
+++ inlab_plasmic/sync-plasmic.sh
@@ -7,7 +7,7 @@ set -Ee
 
 # plasmic.auth file is required in root directory
 
-PLASMIC_PROJECT_ID='wjafXWEvDytFogT7SiMy2v'
+PLASMIC_PROJECT_ID='pDVctTiQaLAwARu4kxRew3'
 
 echo -e "\n----------Syncing Plasmic----------"
 
