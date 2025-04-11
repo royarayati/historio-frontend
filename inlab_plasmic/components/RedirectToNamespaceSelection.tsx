a208470b298c4a4fafb20468a7425f83d551fe2f
royarayati <royarayati1379@gmail.com>
2025-04-11 20:22:55 +0330

    add print patient all consults

--- inlab_plasmic/components/RedirectToNamespaceSelection.tsx
+++ inlab_plasmic/components/RedirectToNamespaceSelection.tsx
@@ -40,7 +40,7 @@ function RedirectToNamespaceSelection(
   // By default, we are just piping all RedirectToNamespaceSelectionProps here, but feel free
   // to do whatever works for you.
 
-  return <PlasmicRedirectToNamespaceSelection  />;
+  return <PlasmicRedirectToNamespaceSelection/>;
 }
 
 export default RedirectToNamespaceSelection;
--- inlab_plasmic/next.config.js
+++ inlab_plasmic/next.config.js
@@ -82,3 +82,4 @@ module.exports = withSentryConfig(
     automaticVercelMonitors: true,
   }
 );
+
